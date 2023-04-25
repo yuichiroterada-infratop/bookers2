45b787fd80bd28847ee49d7aaa856db7acf58c0c
EC2 Default User <ec2-user@ip-172-31-13-223.ap-northeast-1.compute.internal>
2023-04-25 05:23:14 +0000

    [fin]headerとfooterの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, length: {minimum: 2, maximum: 20}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
+  
+  has_many :books, dependent: :destroy
+  has_one_attached :profile_image
+  
+  
+  def get_profile_image(size)
+    if profile_image.attached?
+      profile_image.varient(resize: size).processed
+    else
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/png')
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :users, only: [:show, :edit]
+  resources :books, only: [:index, :show, :edit]
 end
